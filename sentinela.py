457214c2df6cddec3a1d33d98cb1546656e497af
profTomaz <naoroliveira@prof.educacao.sp.gov.br>
2026-04-29 14:04:26 +0000

    v.1

--- sentinela.py
+++ sentinela.py
@@ -1,1 +1,7 @@
-
+#--Variável Sentinela--
+while True:
+    comando = input("Digite o Comando, Se precisar Parar digite: Sair")
+    if comando == "sair":
+        break
+    print(f"Executando:{comando}")
+    
