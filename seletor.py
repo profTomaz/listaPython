45645d7eccd10bed8a682ab685362f853e1bc059
profTomaz <naoroliveira@prof.educacao.sp.gov.br>
2026-05-04 07:51:46 -0300

    Update seletor.py

--- seletor.py
+++ seletor.py
@@ -1,1 +1,8 @@
-
+#Seletor de Caracteres--
+letra = input("Digite uma Letra").upper()
+if letra == 'A':
+  print("Você Selecionou a Opção A")
+elif letra == 'B':
+  print("Você Selecionou a Opção B")
+else:
+  print("Essa Opção não Está disponível")
